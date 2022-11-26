5c9d2a59023f18b70267e65aa1a052a5040d1fe7
Vladimir <vladxtreame@gmail.com>
2022-11-26 17:51:33 +0400

    new homework

---
+++ Homework2seminar/10task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Пример: 456 -> 5; 782 -> 8; 918 -> 1.
+
+
+Console.Clear();
+
+Console.Write("Введите число : ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+string str = i.ToString();
+Console.WriteLine(str[1]);
+
---
+++ Homework2seminar/15task/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Пример: 6 -> да; 7 -> да; 1 -> нет.
+
+Console.Write("Введите день недели ");
+                try
+                {
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    switch (a)
+                    {
+                        case 1: Console.WriteLine("Нет");
+                            break;
+                        case 2: Console.WriteLine("Нет");
+                            break;
+                        case 3: Console.WriteLine("Нет");
+                            break;
+                        case 4: Console.WriteLine("Нет");
+                            break;
+                        case 5: Console.WriteLine("Нет");
+                            break;
+                        case 6: Console.WriteLine("Да");
+                            break;
+                        case 7: Console.WriteLine("Да");
+                            break;
+                        default:
+                            Console.WriteLine("Такого дня недели нет");
+                            break;
+                    }
+                }
+            catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
