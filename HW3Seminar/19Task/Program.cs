128f8a92127ab722c9014a6e1a095c915c5a2884
Vladimir <vladxtreame@gmail.com>
2022-12-02 16:54:38 +0400

    add hw info

---
+++ HW3Seminar/19Task/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+{
+    Console.Write("Введите число: ");
+    uint a = uint.Parse(Console.ReadLine());
+    uint b = a;
+    var col = new List<uint>();
+    while (b > 0)
+    {
+        col.Add(b % 10);
+        b = b / 10;
+    }
+    b = 0;
+    col.Reverse();
+    for (int mcol = 0; mcol < col.Count; mcol++)
+        b = b + col[mcol] * (uint)Math.Pow(10, mcol);
+    if (a == b)
+        Console.WriteLine("Yes");
+    else
+        Console.WriteLine("No");
+}
---
+++ HW3Seminar/23Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход 2 числа (N,M) и выдаёт таблицу кубов чисел от N до M.
+// 2,3 -> 8, 27
+// 1,5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+while (a <= b)
+{
+    Console.WriteLine((a * a * a));
+    a++;
+}
